f887562848e51871a11a2d4a3d0cd8828e2cf94f
Juan Manuel Fernandez <100367452+JuanManuelFernandezV@users.noreply.github.com>
2022-12-29 13:37:23 -0300

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -14,7 +14,7 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('JuanManuelFernandezV/jenkins-nodejs')
+            repositoryName('juanmanuelfernandez1/curso-jenkins')
             tag('${GIT_REVISION,length=7}')
             registryCredentials('docker-hub')
             forcePull(false)
